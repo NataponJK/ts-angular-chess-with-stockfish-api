82f781c81956f273892f7aafff4f06e657091c47
NataponJ <nataponj@gmail.com>
2024-09-11 18:18:16 +0700

    finished abstract piece class

---
+++ src/app/chess-logic/models.ts
@@ -0,0 +1,24 @@
+export enum Color {
+  White,
+  Black,
+}
+
+export type Coords = {
+  x: number;
+  y: number;
+}
+
+export enum FENChar{
+    WhitePawn="White_Pawn",
+    WhiteKnight="White_Knight",
+    WhiteBishop="White_Bishop",
+    WhiteRook="White_Rook",
+    WhiteQueen="White_Queen",
+    WhiteKing="White_King",
+    BlackPawn="Black_Pawn",
+    BlackKnight="Black_Knight",
+    BlackBishop="Black_Bishop",
+    BlackRook="Black_Rook",
+    BlackQueen="Black_Queen",
+    BlackKing="Black_King",
+}
